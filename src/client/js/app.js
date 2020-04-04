0f69b24458ad0217fbdc74183e33b8067469cceb
jamiekim828 <jamiekim828@gmail.com>
2020-04-04 19:25:25 +0200

    fetch destination data from geonames

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,28 @@
-function performAction(e) {}
+document.getElementById('planbutton').addEventListener('click', performAction);
+
+function performAction(e) {
+  let cityname = document.getElementById('destination').value;
+
+  if (cityname) {
+    getLocation();
+  }
+}
+
+// Geonames API
+let geonames_baseURL = 'http://api.geonames.org/searchJSON?q=';
+let geonames_Key = `&maxRows=10&username=jamiekim828`;
+
+const getLocation = async () => {
+  const input = document.getElementById('destination').value;
+
+  const res = await fetch(`${geonames_baseURL}+${input}+${geonames_Key}`);
+  try {
+    const location = await res.json();
+    console.log('location', location);
+    return location;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
 
 export { performAction };
